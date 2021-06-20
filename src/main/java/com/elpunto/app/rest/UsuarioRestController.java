c77095424bb1c06f1db5f4dcbbe52abd0a7cc252
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-06-20 09:00:11 -0500

    Agregando metodo get para obtener usuario por id

--- src/main/java/com/elpunto/app/rest/UsuarioRestController.java
+++ src/main/java/com/elpunto/app/rest/UsuarioRestController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,12 +22,31 @@ public class UsuarioRestController {
 
 	@Autowired
 	IUsuarioService usuarioService;
-	
+
 	@GetMapping
-	public ResponseEntity<Map<String,Object>> listarUsuarios(){
-		Map<String,Object> response = new HashMap<>();
+	public ResponseEntity<Map<String, Object>> listarUsuarios() {
+		Map<String, Object> response = new HashMap<>();
 		List<Usuario> lstUsuarios = usuarioService.listarUsuarios();
 		response.put("usuarios", lstUsuarios);
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> obtenerUsuario(@PathVariable Integer id) {
+		Usuario usuario = null;
+		Map<String, Object> response = new HashMap<>();
+		try {
+			usuario = usuarioService.buscarUsuario(id);
+			if(usuario == null) {
+				response.put("mensaje", "El usuario con id: " + id.toString() + " no existe en la base de datos");
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+			}else {
+				return new ResponseEntity<>(usuario, HttpStatus.OK);
+			}
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta a la base de datos.");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}	
 	}
 }
