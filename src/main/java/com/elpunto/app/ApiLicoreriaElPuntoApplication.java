0499235b16dddc09538cd8cbaa71987f621b3cd9
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-06-19 21:54:55 -0500

    Creando API para Licorería

---
+++ src/main/java/com/elpunto/app/ApiLicoreriaElPuntoApplication.java
@@ -0,0 +1,13 @@
+package com.elpunto.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLicoreriaElPuntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLicoreriaElPuntoApplication.class, args);
+	}
+
+}
