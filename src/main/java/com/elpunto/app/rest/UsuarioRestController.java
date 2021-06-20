f6ac0d1bac7b997775f203800e1547d4e1096f88
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-06-20 09:11:41 -0500

    Metodo registrar y actualizar listos

--- src/main/java/com/elpunto/app/rest/UsuarioRestController.java
+++ src/main/java/com/elpunto/app/rest/UsuarioRestController.java
@@ -10,6 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,16 +40,64 @@ public class UsuarioRestController {
 		Map<String, Object> response = new HashMap<>();
 		try {
 			usuario = usuarioService.buscarUsuario(id);
-			if(usuario == null) {
+			if (usuario == null) {
 				response.put("mensaje", "El usuario con id: " + id.toString() + " no existe en la base de datos");
 				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
-			}else {
+			} else {
 				return new ResponseEntity<>(usuario, HttpStatus.OK);
 			}
 		} catch (DataAccessException e) {
 			response.put("mensaje", "Error al realizar la consulta a la base de datos.");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-		}	
+		}
+	}
+
+	@PostMapping("/registro")
+	public ResponseEntity<?> crearUsuario(@RequestBody Usuario u) {
+		Usuario nuevoUsuario = null;
+		Map<String, Object> response = new HashMap<>();
+		try {
+			nuevoUsuario = usuarioService.guardarUsuario(u);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta a la base de datos.");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje", "El usuario fue creado con éxito");
+		response.put("usuario", nuevoUsuario);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+
+	@PutMapping("/editar/{id}")
+	public ResponseEntity<?> actualizarUsuario(@RequestBody Usuario u, @PathVariable Integer id) {
+		Usuario usuarioActual = usuarioService.buscarUsuario(id);
+		Usuario usuarioActualizado = null;
+		Map<String, Object> response = new HashMap<>();
+		if (usuarioActual == null) {
+			response.put("mensaje",
+					"No se pudo editar, el usuario con el id " + id.toString() + " no existe en la base de datos");
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		} else {
+			try {
+				usuarioActual.setEmail(u.getEmail());
+				usuarioActual.setPassword(u.getPassword());
+				usuarioActual.setDni(u.getDni());
+				usuarioActual.setNombres(u.getNombres());
+				usuarioActual.setApellidos(u.getApellidos());
+				usuarioActual.setTelefono(u.getTelefono());
+				usuarioActual.setRol(u.getRol());
+				
+				usuarioActualizado = usuarioService.guardarUsuario(usuarioActual);
+			} catch (DataAccessException e) {
+				response.put("mensaje", "Error al realizar el registro a la base de datos.");
+				response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			response.put("mensaje", "El usuario fue actualizado con éxito");
+			response.put("usuario", usuarioActualizado);
+
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+		}
 	}
 }
