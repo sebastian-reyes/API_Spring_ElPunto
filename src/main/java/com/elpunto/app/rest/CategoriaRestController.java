66ed7910020ae3661a5085148e4bd83583142f50
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-08-14 10:42:00 -0500

    Agregando método para registrar categoría

--- src/main/java/com/elpunto/app/rest/CategoriaRestController.java
+++ src/main/java/com/elpunto/app/rest/CategoriaRestController.java
@@ -16,6 +16,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -80,6 +82,27 @@ public class CategoriaRestController {
 			}
 		} catch (DataAccessException e) {
 			response.put("mensaje", "Error al realizar la consulta en la base de datos.");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PostMapping("/registro")
+	public ResponseEntity<?> crearCategoria(@RequestBody Categoria c) {
+		Categoria categoriaNueva = null;
+		Map<String, Object> response = new HashMap<>();
+		try {
+			if (c.getNombre_cat().replace(" ", "").length() > 0) {
+				categoriaNueva = categoriaService.guardarCategoria(c);
+				response.put("mensaje", "La categoría fue creada con éxito");
+				response.put("categoria", categoriaNueva);
+				return new ResponseEntity<Map<String,Object>>(response, HttpStatus.CREATED);
+			} else {
+				response.put("mensaje", "No pueden haber campos vacíos.");
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+			}
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar el registro a la base de datos.");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
